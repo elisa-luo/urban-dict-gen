78381a11d15ea9abf83fe3f35f2646020bac268c
Elisa Luo <eyl2130@columbia.edu>
2020-05-22 20:39:06 -0700

    starting code

--- write.py
+++ write.py
@@ -8,17 +8,20 @@ Created on Fri May 22 16:13:53 2020
 
 import pandas as pd
 
-out = open('defs.txt', 'w')
+out = open('good_defs.txt', 'w')
 
 data = pd.read_csv('urbandict-word-defs.csv', error_bad_lines=False)
+
 
 defs = data['definition']
 words = data['word']
 
 for index, row in data.iterrows():
-    out.write(str(row["word"]))
-    out.write(": ")
-    out.write(str(row["definition"]))
-    out.write("\n\n")
+    #get rid of bad entries
+    if row["up_votes"] > row["down_votes"]*4:
+        out.write(str(row["word"]))
+        out.write(": ")
+        out.write(str(row["definition"]))
+        out.write("\n\n")
     
 print("\n\n done")
